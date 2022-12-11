3941cd8c03afe691414c4545b9cd7cc73777a7d6
2Tough <vainferro@gmail.com>
2022-12-11 15:52:33 +0300

    Added hitboxes with colored rectangles.

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+
+const collisionCanvas = document.getElementById('collisionCanvas')
+const collisionCtx = canvas.getContext('2d')
+collisionCanvas.width = window.innerWidth
+collisionCanvas.height = window.innerHeight
+
+
 let timeToNextRaven = 0
 let ravenInterval = 500
 let lastTime = 0
+let score = 0
+ctx.font = '50px Impact'
 
 let ravens = []
 class Raven {
@@ -24,21 +33,46 @@ class Raven {
         this.image.src = './images/raven.png'
         this.frame = 0
         this.maxFrame = 4
-        
+        this.timeSinceFlap = 0
+        this.flapInterval = Math.random() * 50 + 50
+        this.randomColors = [Math.random(Math.random * 255), Math.random(Math.random * 255), Math.random(Math.random * 255)]
+        this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')'
     }
-    update() {
+    update(deltatime) {
+        if (this.y < 0 || this.y > canvas.height - this.height) {
+            this.directionY = this.directionY * -1
+        }
         this.x -= this.directionX
+        this.y += this.directionY
         if (this.x < 0) this.markedForDeletion = true
-        if (this.frame > this.maxFrame) this.frame = 0
-        else this.frame++
+        this.timeSinceFlap += deltatime
+        if(this.timeSinceFlap > this.flapInterval) {
+            if (this.frame > this.maxFrame) this.frame = 0
+            else this.frame++
+            this.timeSinceFlap = 0
+        }
+
     }
     draw() {
+        ctx.fillStyle = this.color;
         ctx.strokeRect(this.x, this.y, this.width, this.height)
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
 }
 
+function drawScore() {
+    ctx.fillStyle = 'black'
+    ctx.fillText('Score: ' + score, 50, 75);
+    ctx.fillStyle = 'white'
+    ctx.fillText('Score: ' + score, 55, 85);
+}
+
 const raven = new Raven()
+
+window.addEventListener('click', function(e) {
+    const detectPixelColor = ctx.getImageData(e.x, e.y, 1, 1)
+    console.log(detectPixelColor)
+})
 
 function animate(timestamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -49,7 +83,8 @@ function animate(timestamp) {
         ravens.push(new Raven())
         timeToNextRaven = 0
     }
-    [...ravens].forEach(object => object.update());
+    drawScore();
+    [...ravens].forEach(object => object.update(deltatime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion)
     requestAnimationFrame(animate)
